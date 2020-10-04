ec0218cac61289cce94c599cd20a2dff8f5163d6
Anupam Manna <30969673+its7080@users.noreply.github.com>
2020-10-04 08:59:50 +0530

    a timestamp added at dynamic side

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const { request, response } = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+const app = express();
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
